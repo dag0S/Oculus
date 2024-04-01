7d4d0eeae8fd5919e1cc05790138eca8d1470d5c
Gosudarev Danila <danidagosudarev@gmail.com>
2024-04-01 21:46:08 +0300

    fixed gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,55 @@
-const {src, dest, watch, parallel, series} = require('gulp');
+const { src, dest, watch, parallel, series } = require("gulp");
 
-const scss         = require('gulp-sass')(require('sass')),
-      concat       = require('gulp-concat'),
-      uglify       = require('gulp-uglify-es').default,
-      browserSync  = require('browser-sync').create(),
-      autoprefixer = require('gulp-autoprefixer'),
-      clean        = require('gulp-clean');
+const scss = require("gulp-sass")(require("sass")),
+  concat = require("gulp-concat"),
+  uglify = require("gulp-uglify-es").default,
+  browserSync = require("browser-sync").create(),
+  autoprefixer = require("gulp-autoprefixer"),
+  clean = require("gulp-clean");
 
 function scripts() {
-    return src('app/js/main.js')
-        .pipe(concat('main.min.js'))                    
-        .pipe(uglify())
-        .pipe(dest('app/js'))
-        .pipe(browserSync.stream());
+  return src("app/js/main.js")
+    .pipe(concat("main.min.js"))
+    .pipe(uglify())
+    .pipe(dest("app/js"))
+    .pipe(browserSync.stream());
 }
 
 function styles() {
-    return src('app/scss/style.scss')
-        .pipe(autoprefixer({overrideBrowserslist: ['last 10 version']}))
-        .pipe(concat('style.min.css'))
-        .pipe(scss({outputStyle: 'compressed'}))
-        .pipe(dest('app/css'))
-        .pipe(browserSync.stream());
+  return src("app/scss/style.scss")
+    .pipe(autoprefixer({ overrideBrowserslist: ["last 10 version"] }))
+    .pipe(concat("style.min.css"))
+    .pipe(scss({ outputStyle: "compressed" }))
+    .pipe(dest("app/css"))
+    .pipe(browserSync.stream());
 }
 
 function watching() {
-    browserSync.init({
-        server: {
-            baseDir: "app/"
-        }
-    });
-    watch(['app/scss/style.scss'], styles);
-    watch(['app/js/main.js'], scripts);
+  browserSync.init({
+    server: {
+      baseDir: "app/",
+    },
+  });
+  watch(["app/scss/style.scss"], styles);
+  watch(["app/js/main.js"], scripts);
 }
 
 function cleanDist() {
-    return src('dist')
-        .pipe(clean());
+  return src("dist").pipe(clean());
 }
 
 function building() {
-    return src([
-        'app/css/style.min.css',
-        'app/img/dist/*.*',
-        'app/js/main.min.js',
-        'app/**/*.html'
-    ], {base: 'app'})
-        .pipe(dest('dist'));
+  return src(
+    [
+      "app/css/style.min.css",
+      "app/img/*.*",
+      "app/icons/*.*",
+      "app/fonts/*.*",
+      "app/js/main.min.js",
+      "app/**/*.html",
+    ],
+    { base: "app" }
+  ).pipe(dest("dist"));
 }
 
 exports.styles = styles;
